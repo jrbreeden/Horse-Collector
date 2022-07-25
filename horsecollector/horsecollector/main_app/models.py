7d7db917789f6508bac92ae032438d4568ade7c4
jrbreeden <jrbreeden55@gmail.com>
2022-07-25 14:25:38 -0400

    Create Model

--- horsecollector/horsecollector/main_app/models.py
+++ horsecollector/horsecollector/main_app/models.py
@@ -1,16 +1,8 @@
 from django.db import models
 
 # Create your models here.
-# Add the Cat class & list and view function below the imports
-class Horse:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-horses = [
-  Horse('Paint', 'American Paint', 'friendly', 3),
-  Horse('Spot', 'Appaloosa', 'fiery', 0),
-  Horse('Blacky', 'American Quarter', 'runner', 4)
-]
+class Horse(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
--- horsecollector/horsecollector/main_app/views.py
+++ horsecollector/horsecollector/main_app/views.py
@@ -3,7 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 
+# Add the Horse class & list and view function below the imports
+class Horse:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
 
+horses = [
+  Horse('Paint', 'American Paint', 'friendly', 3),
+  Horse('Spot', 'Appaloosa', 'fiery', 0),
+  Horse('Blacky', 'American Quarter', 'runner', 4)
+]
 
 
 

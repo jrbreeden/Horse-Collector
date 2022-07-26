15c56f77baabd96531c0437697ca514032eb0671
jrbreeden <jrbreeden55@gmail.com>
2022-07-26 09:18:50 -0400

    Add meals

--- horsecollector/horsecollector/main_app/models.py
+++ horsecollector/horsecollector/main_app/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
 
-# Create your models here.
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
+
+
+
+
+
 class Horse(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -9,3 +20,18 @@ class Horse(models.Model):
 
     def __str__(self):
       return self.name
+
+
+
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+    )
+
+  horse = models.ForeignKey(Horse, on_delete=models.CASCADE)
+    
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"

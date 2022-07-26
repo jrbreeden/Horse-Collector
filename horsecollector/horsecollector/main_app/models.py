06f7dbffa1cd357647c39c33f53c8256dc0008d1
jrbreeden <jrbreeden55@gmail.com>
2022-07-26 12:24:32 -0400

    Add Feeding Model

--- horsecollector/horsecollector/main_app/models.py
+++ horsecollector/horsecollector/main_app/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
-
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
 
 
 
@@ -19,7 +23,15 @@ class Horse(models.Model):
     def get_absolute_url(self):
       return reverse('detail', kwargs={'horse_id': self.id})
 
-
-
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS [0] [0]
+    )
+    horse = models.ForeignKey(Horse, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
 

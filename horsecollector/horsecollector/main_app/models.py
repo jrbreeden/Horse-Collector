d987ab1c97f808f58ad05960dd58a3c508210cd0
jrbreeden <jrbreeden55@gmail.com>
2022-07-26 15:14:13 -0400

    Fed_for_today works

--- horsecollector/horsecollector/main_app/models.py
+++ horsecollector/horsecollector/main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
   ('B', 'Breakfast'),
@@ -23,6 +24,9 @@ class Horse(models.Model):
     def get_absolute_url(self):
       return reverse('detail', kwargs={'horse_id': self.id})
 
+    def fed_for_today(self):
+      return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
 class Feeding(models.Model):
     date = models.DateField('feeding date')
     meal = models.CharField(
@@ -34,4 +38,7 @@ class Feeding(models.Model):
 
     def __str__(self):
         return f"{self.get_meal_display()} on {self.date}"
+    
+    class Meta:
+      ordering = ['-date']
 
--- horsecollector/horsecollector/main_app/views.py
+++ horsecollector/horsecollector/main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from .models import Horse
@@ -20,6 +20,19 @@ def horses_detail(request, horse_id):
   horse = Horse.objects.get(id=horse_id)
   feeding_form = FeedingForm()
   return render(request, 'horses/detail.html', { 'horse': horse, 'feeding_form': feeding_form })
+
+def add_feeding(request, horse_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the horse_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.horse_id = horse_id
+    new_feeding.save()
+  return redirect('detail', horse_id=horse_id)
+
 
 class HorseCreate(CreateView):
   model = Horse

bac7ccda3be714e842e7ae6b2c92729809dd3446
jrbreeden <jrbreeden55@gmail.com>
2022-07-27 11:17:51 -0400

    Create Signup

--- horsecollector/horsecollector/main_app/views.py
+++ horsecollector/horsecollector/main_app/views.py
@@ -6,6 +6,8 @@ from .forms import FeedingForm
 import uuid
 import boto3
 import os
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def home(request):
@@ -105,3 +107,17 @@ def add_photo(request, horse_id):
      print('An error occurred uploading file to S3')
      print(e)
   return redirect('detail', horse_id=horse_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
